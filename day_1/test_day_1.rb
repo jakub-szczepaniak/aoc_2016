eafa12d708c19e56da9bfa55ced58e40e80eb0fb
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2016-12-01 21:09:57 +0100

    we now calculate distance between two identical points

--- day_1/test_day_1.rb
+++ day_1/test_day_1.rb
@@ -2,3 +2,9 @@
 gem 'minitest', '>= 5.0.0'
 require 'minitest/autorun'
 require_relative 'day_1.rb'
+
+class TaxiMetricTest < MiniTest::Test
+  def test_distance_between_identical_base_points_is_zero
+    assert_equal 0, d(0, 0, 0, 0)
+  end
+end

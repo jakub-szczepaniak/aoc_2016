b9f3159470525cc04974b15e475db2c0393dd989
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2017-01-04 18:44:29 +0100

    add methods to turn left and right

--- day_1/day_1.rb
+++ day_1/day_1.rb
@@ -19,19 +19,44 @@ class Position
 
   def go(map_direction)
     turn, steps = map_direction.match(/(.)(.+)/).captures
-    if turn == 'R' && face == 'North'
-      @face = 'East'
-      multi = 1
-    elsif turn == 'L' && face == 'South'
-      @face = 'East'
-      multi = 1
-    elsif turn == 'L' && face == 'North'
-      @face = 'West'
-      multi = -1
-    elsif turn == 'R' && face == 'South'
-      @face = 'West'
-      multi = -1
+
+    if turn == 'R' 
+      multi = turn_right
+    elsif turn == 'L' 
+      multi = turn_left
     end
-    @location = Location.new(multi * steps.to_i + @location.x, 0)
+    @location = Location.new(
+      multi[0] * steps.to_i + @location.x,
+      multi[1] * steps.to_i + @location.y)
+  end
+  def turn_right
+    if @face == 'North'
+      @face = 'East'
+      return [1, 0]
+    elsif @face == 'East'
+      @face = 'South'
+      return [0, -1]
+    elsif @face == 'South'
+      @face = 'West'
+      return [-1, 0]
+    elsif @face == 'West'
+      @face = 'North'
+      return [0, 1]
+    end
+  end
+  def turn_left
+    if @face == 'North'
+      @face = 'West'
+      return [-1, 0]
+    elsif @face == 'East'
+      @face = 'North'
+      return [0, 1]
+    elsif @face == 'South'
+      @face = 'East'
+      return [1, 0]
+    elsif @face == 'West'
+      @face = 'South'
+      return [0, -1]
+    end
   end
 end

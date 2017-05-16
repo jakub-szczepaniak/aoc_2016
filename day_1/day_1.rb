98f40b09ca94adbcdde87c4f69a46ef4f9fdfffe
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2017-05-16 22:48:17 +0200

    refactor main class file

--- day_1/day_1.rb
+++ day_1/day_1.rb
@@ -1,4 +1,4 @@
-class Coordinate
+class Coordinates
   attr_reader :x, :y
   def initialize(x, y)
     @x = x
@@ -12,54 +12,53 @@ end
 
 class Position
   attr_reader :location, :face, :locations
-  def initialize(face = 'North')
-    @location = Coordinate.new(0, 0)
+  FACES = {
+    'North' => [0, 1],
+    'East' => [1, 0],
+    'West' => [-1, 0],
+    'South' => [0, -1]}
+  def initialize(face = 'North', tracker = DefaultTracker.new)
+    @location = Coordinates.new(0, 0)
     @face = face
     @locations = [@location]
   end
 
   def go(map_direction)
     turn, steps = map_direction.match(/(.)(.+)/).captures
-
     if turn == 'R' 
       multi = turn_right
     elsif turn == 'L' 
       multi = turn_left
     end
-    @location = Coordinate.new(
+    @tracker.track(@location, steps.to_i, multi)
+    @location = Coordinates.new(
       multi[0] * steps.to_i + @location.x,
       multi[1] * steps.to_i + @location.y)
+
   end
 
   def turn_right
     if @face == 'North'
       @face = 'East'
-      return [1, 0]
     elsif @face == 'East'
       @face = 'South'
-      return [0, -1]
     elsif @face == 'South'
       @face = 'West'
-      return [-1, 0]
     elsif @face == 'West'
       @face = 'North'
-      return [0, 1]
     end
+    FACES[@face]
   end
-  
+
   def turn_left
     if @face == 'North'
       @face = 'West'
-      return [-1, 0]
     elsif @face == 'East'
       @face = 'North'
-      return [0, 1]
     elsif @face == 'South'
       @face = 'East'
-      return [1, 0]
     elsif @face == 'West'
       @face = 'South'
-      return [0, -1]
     end
   end
 end

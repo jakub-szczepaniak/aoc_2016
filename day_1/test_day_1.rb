6d348908fd661310fd4e104b4629ae6483b12055
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2017-05-29 21:23:10 +0200

    move Coordinates tests to separate module

--- day_1/test_day_1.rb
+++ day_1/test_day_1.rb
@@ -3,39 +3,6 @@ gem 'minitest', '>= 5.0.0'
 require 'minitest/autorun'
 require_relative 'day_1.rb'
 
-class TaxiMetricTest < MiniTest::Test
-  def test_distance_between_identical_base_points_is_zero
-    location = Coordinates.new(0, 0)
-    assert_equal 0, location.taxi(location)
-  end
-
-  def test_distance_between_points_moved_on_y_axis_is_one
-    location = Coordinates.new(0, 0)
-
-    assert_equal 1, location.taxi(Coordinates.new(0, 1))
-  end
-
-  def test_distance_between_points_moved_on_x_axis_is_one
-    location = Coordinates.new(1, 0)
-    assert_equal 1, location.taxi(Coordinates.new(0, 0))
-  end
-
-  def test_distance_between_1_1_and_0_0_is_two
-    location = Coordinates.new(1, 1)
-    assert_equal 2, location.taxi(Coordinates.new(0, 0))
-  end
-  def test_equality_between_same_location
-    location1 = Coordinates.new(0, 0)
-    location2 = Coordinates.new(0, 0)
-    assert_equal true,  location1 == location2
-  end
-  def test_not_equal_for_different_coordinates
-    location1 = Coordinates.new(0, 1)
-    location2 = Coordinates.new(1, 0)
-    assert_equal false,  location1 == location2
-  end
-end
-
 class TestFollowingDirections < MiniTest::Test
   def test_initial_position_is_0_0
     landing = Position.new

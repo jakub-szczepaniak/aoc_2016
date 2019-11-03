aaf9b579f6cf2fa7045d3ae2f2f96aa32f42847a
Jakub Szczepaniak <szczepaniak.jkb@gmail.com>
2019-11-03 21:55:26 +0100

    Day_1. Parsing input into directions and steps

--- day_1/day_1.tcl
+++ day_1/day_1.tcl
@@ -6,9 +6,17 @@ set puzzle_input_id [open day_1.input r]
 
 gets $puzzle_input_id puzzle_input
 
+set current_x 0
+set current_y 0
+set current_face N
+
+
+
 set steps [split $puzzle_input ","]
 foreach { step }  $steps  {
-	puts  $step
+	lassign [split [string trim $step] ""] turn steps
+	puts  "turn $turn and go $steps steps"
+	
 }
 
 close $puzzle_input_id

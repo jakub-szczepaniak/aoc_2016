061d5227932bf7c56cda36757b465961e9354df1
Jakub Szczepaniak <szczepaniak.jkb@gmail.com>
2019-11-03 21:41:05 +0100

    Day_1. Reads input and splits it into steps.

--- day_1/day_1.tcl
+++ day_1/day_1.tcl
@@ -1,3 +1,14 @@
 proc to_be_tested {} {
 	return true
 }
+
+set puzzle_input_id [open day_1.input r]
+
+gets $puzzle_input_id puzzle_input
+
+set steps [split $puzzle_input ","]
+foreach { step }  $steps  {
+	puts  $step
+}
+
+close $puzzle_input_id

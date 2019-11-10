543823486d8cc5a5114911bdbc63cb13d23d7f2f
Jakub Szczepaniak <szczepaniak.jkb@gmail.com>
2019-11-10 21:38:25 +0100

    Day_1. Turning right is now handled.

--- day_1/day_1_proc.tcl
+++ day_1/day_1_proc.tcl
@@ -3,6 +3,13 @@ proc to_be_tested {} {
 }
 
 proc move { location direction} {
-	set result [dict create x 0 y 0 face E]
+	lassign [split $direction ""] vector _
+	
+	if {$vector == {R}} {
+		set result [dict create x 0 y 0 face E]
+	} else {
+		set result [dict create x 0 y 0 face W]		
+	}
+
 	return $result
 }

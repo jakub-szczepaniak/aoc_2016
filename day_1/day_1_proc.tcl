f5f695e436ca2fb83bb21dae755d6161dbcfdf0a
Jakub Szczepaniak <szczepaniak.jkb@gmail.com>
2019-11-12 22:56:51 +0100

    Day_1. Adds multiplication for complex numbers to help with vector manipulation.

--- day_1/day_1_proc.tcl
+++ day_1/day_1_proc.tcl
@@ -7,9 +7,23 @@ proc move { position direction directive} {
 	
 	if {$vector == {R}} {
 		set result [dict create x 0 y 0 face E]
+		set new_direction [dict create x 1 y 0]
 	} else {
-		set result [dict create x 0 y 0 face W]		
+		set result [dict create x 0 y 0 face W]
+		set new_direction [dict create x -1 y 0]		
 	}
 
 	return $result
+}
+
+proc complex { real imaginary} {
+	return [list $real $imaginary]
+}
+
+proc multiply { first second} {
+	lassign $first x1 y1
+	lassign $second	x2 y2
+	set new_first [expr ($x1*$x2) - ($y1*$y2)]
+	set new_second [expr $x1*$y2 + $x2*$y1]
+	return [complex $new_first $new_second]
 }
